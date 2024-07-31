8fb1ed3f966686464b8d46c515c26d2cf3281043
nischal <nischalkshaj5@gmail.com>
2024-07-31 17:47:09 +0530

    change:17 in the yml file for the deployment

--- src/infrastructure/database/mongoClient.js
+++ src/infrastructure/database/mongoClient.js
@@ -6,8 +6,21 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB);
-    console.log("mongodb connected");
+    // Determine which URI to use based on the environment
+    const mongoURI =
+      process.env.NODE_ENV === "production"
+        ? process.env.MONGODB_CLOUD_URI
+        : process.env.MONGODB_LOCAL_URI;
+
+    if (!mongoURI) {
+      throw new Error(
+        "MongoDB URI is not defined in the environment variables"
+      );
+    }
+
+    await mongoose.connect(mongoURI);
+
+    console.log("MongoDB connected");
   } catch (error) {
     console.error("error while connecting the mongodb", error);
     process.exit(1);
